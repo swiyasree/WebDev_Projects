250c0162c6afda82b0f88856a7bd5583ee5fd2c9
Sree209 <ssyarlag@usc.edu>
2024-02-13 14:28:28 -0800

    Push changes of html file name

--- HW2/mediate.py
+++ HW2/mediate.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('HW2.html')
 
 @app.route('/get_stock_data')
 def get_stock_data():
